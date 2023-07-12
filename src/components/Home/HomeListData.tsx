fcf37da3f4dee6bf30d2a435e077ca62c57a463c
Nikita Karande <nikitakarande85@gmail.com>
2023-07-12 10:27:28 +0530

    Add changes for web-portal-dev

--- src/components/Home/HomeListData.tsx
+++ src/components/Home/HomeListData.tsx
@@ -510,7 +510,7 @@ export default function HomeListData({
 											// }}
 											calloutProps={{
 												className: `${
-													themeName == 'Light'
+													themeName === 'Light'
 														? 'datePickerCalloutLight'
 														: 'datePickerCalloutDark'
 												}`,
